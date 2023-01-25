8ba9ef9338ee7154fb72b7f5d90e874d2dc8fa6f
Yohei Okabayashi <yohei.kuro48@gmail.com>
2023-01-25 23:23:52 +0900

    FEAT: add table of contents

---
+++ app/src/components/organism/TableOfContents/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react'
+import { parse } from 'node-html-parser'
+import TableOfContentHeading from '@/components/atoms/TableOfContentHeading'
+
+type TableOfContentsProps = {
+  path: string
+  htmlSource: string
+}
+
+const TableOfContents: FC<TableOfContentsProps> = (props) => {
+  const { path, htmlSource } = props
+  const root = parse(htmlSource)
+  const h2Nodes = root.querySelectorAll('h2')
+
+  return (
+    <nav className="m-5 flex flex-col gap-3 rounded-lg p-3" aria-label="toc">
+      <div className="text-base font-bold text-gray-600">TABLE OF CONTENTS</div>
+      <ul className="list-disc">
+        {h2Nodes.map((h2Node) => (
+          <TableOfContentHeading
+            key={h2Node.id}
+            path={path}
+            headingNode={h2Node}
+          />
+        ))}
+      </ul>
+    </nav>
+  )
+}
+
+export default TableOfContents
